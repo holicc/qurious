85baa828a2b9a58da1d2e10c73fa83aea1f543d5
joe <longshanluu@gmail.com>
2024-01-25 20:32:07 +0800

    Add Aggregate struct and implementation

---
+++ src/logical_plan/aggregate.rs
@@ -0,0 +1,41 @@
+use std::{fmt::Display, sync::Arc};
+
+use crate::{expr::LogicalExpr, types::schema::Schema};
+
+use super::LogicalPlan;
+
+pub struct Aggregate {
+    schema: Schema,
+    input: Arc<dyn LogicalPlan>,
+    group_expr: Vec<Box<dyn LogicalExpr>>,
+    aggr_expr: Vec<Box<dyn LogicalExpr>>,
+}
+
+impl LogicalPlan for Aggregate {
+    fn schema(&self) -> &Schema {
+        &self.schema
+    }
+
+    fn children(&self) -> Option<Vec<&dyn LogicalPlan>> {
+        Some(vec![&*self.input])
+    }
+}
+
+impl Display for Aggregate {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Aggregate: group_expr={}, aggregat_expr={}",
+            self.group_expr
+                .iter()
+                .map(|f| f.to_string())
+                .collect::<Vec<String>>()
+                .join(","),
+            self.aggr_expr
+                .iter()
+                .map(|f| f.to_string())
+                .collect::<Vec<String>>()
+                .join(",")
+        )
+    }
+}
