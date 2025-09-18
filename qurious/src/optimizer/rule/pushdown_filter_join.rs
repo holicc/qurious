8659693fd56c32a21bdf4b8d316e71666141c017
longshan.lu <longshan.lu@longbridge-inc.com>
2025-09-18 10:30:25 +0800

    refactor: Enhance pushdown filter logic to support cross joins and improve test coverage for optimizer rules

--- qurious/src/optimizer/rule/pushdown_filter_join.rs
+++ qurious/src/optimizer/rule/pushdown_filter_join.rs
@@ -1,18 +1,14 @@
-use std::collections::{HashMap, HashSet};
-use std::sync::Arc;
-
-use arrow::datatypes::SchemaRef;
-
 use crate::common::join_type::JoinType;
 use crate::common::table_schema::qualified_name;
-use crate::common::transformed::{TransformNode, Transformed, TransformedResult, TreeNodeRecursion};
-use crate::datatypes::operator::Operator;
+use crate::common::transformed::Transformed;
 use crate::error::Result;
-use crate::logical::expr::{BinaryExpr, Column, LogicalExpr, SubQuery};
-use crate::logical::plan::{CrossJoin, Filter, Join, LogicalPlan, SubqueryAlias};
+use crate::logical::expr::{Column, LogicalExpr};
+use crate::logical::plan::{Filter, Join, LogicalPlan, SubqueryAlias};
 use crate::logical::LogicalPlanBuilder;
 use crate::optimizer::rule::rule_optimizer::OptimizerRule;
-use crate::utils::expr::{conjunction, replace_col, replace_cols, replace_cols_by_name, split_conjunctive_predicates};
+use crate::utils::expr::{conjunction, replace_col, replace_cols_by_name, split_conjunctive_predicates};
+use std::collections::HashMap;
+use std::sync::Arc;
 
 #[derive(Debug, Default, Clone)]
 pub struct PushdownFilter;
@@ -42,6 +38,17 @@ impl OptimizerRule for PushdownFilter {
                 Ok(Transformed::yes(LogicalPlan::TableScan(scan)))
             }
             LogicalPlan::Join(join) => self.push_join(join, Some(&filter.expr)),
+            LogicalPlan::CrossJoin(cross_join) => self.push_join(
+                Join {
+                    left: cross_join.left,
+                    right: cross_join.right,
+                    join_type: JoinType::Inner,
+                    on: vec![],
+                    filter: None,
+                    schema: cross_join.schema,
+                },
+                Some(&filter.expr),
+            ),
             LogicalPlan::SubqueryAlias(subquery_alias) => {
                 let input_schema = subquery_alias.input.table_schema();
                 let mut replace_map = HashMap::new();
@@ -71,8 +78,6 @@ impl OptimizerRule for PushdownFilter {
 }
 
 impl PushdownFilter {
-    fn push_into() {}
-
     fn push_join(&self, mut join: Join, parent_predicate: Option<&LogicalExpr>) -> Result<Transformed<LogicalPlan>> {
         let left_schema = join.left.table_schema();
         let right_schema = join.right.table_schema();
@@ -150,7 +155,9 @@ impl PushdownFilter {
 #[cfg(test)]
 mod tests {
     use crate::{
-        optimizer::rule::{pushdown_filter_join::PushdownFilter, ExtractEquijoinPredicate},
+        optimizer::rule::{
+            eliminate_cross_join::EliminateCrossJoin, pushdown_filter_join::PushdownFilter, ExtractEquijoinPredicate,
+        },
         test_utils::assert_after_optimizer,
     };
 
@@ -220,6 +227,35 @@ mod tests {
                 "      TableScan: users, full_filter=[users.id > Int64(1)]",
                 "    SubqueryAlias: b",
                 "      TableScan: repos, full_filter=[repos.owner_id > Int64(1)]",
+            ],
+        );
+    }
+
+    #[test]
+    fn test_cross_join() {
+        assert_after_optimizer(
+            "SELECT * FROM users, repos WHERE users.id = repos.owner_id AND users.id = 10",
+            vec![Box::new(PushdownFilter)],
+            vec![
+                "Projection: (users.email, repos.id, users.id, repos.name, users.name, repos.owner_id)",
+                "  Filter: users.id = repos.owner_id",
+                "    Inner Join:",
+                "      TableScan: users, full_filter=[users.id = Int64(10)]",
+                "      TableScan: repos",
+            ],
+        );
+    }
+
+    #[test]
+    fn test_cross_join_01() {
+        assert_after_optimizer(
+            "SELECT * FROM users, repos WHERE users.id = repos.owner_id AND users.id = 10",
+            vec![Box::new(EliminateCrossJoin), Box::new(PushdownFilter)],
+            vec![
+                "Projection: (users.email, repos.id, users.id, repos.name, users.name, repos.owner_id)",
+                "  Inner Join: On: (users.id, repos.owner_id)",
+                "    TableScan: users, full_filter=[users.id = Int64(10)]",
+                "    TableScan: repos, full_filter=[repos.owner_id = Int64(10)]",
             ],
         );
     }
